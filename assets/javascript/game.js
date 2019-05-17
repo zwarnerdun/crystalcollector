09dc189ace7c606895d5a06982b160402133dc20
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-05-16 21:33:00 -0400

    added variables to my javascript and a for loop for the crystals when generating random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
 // The player will be shown a random number at the start of the game.
+var randomresult;
+var wins = 0;
+var losses = 0;
+console.log(losses)
 // The random number shown at the start of the game should be between 19 - 120.
 var randomnumber = Math.floor(Math.random() * 120 + 19);
+// 4 crystals
+for (var i = 0; i < 4; i++){
+    console.log("Hello");
+};
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
+// Each crystal should have a random hidden value between 1 - 12.
 
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
@@ -14,10 +22,10 @@ var randomnumber = Math.floor(Math.random() * 120 + 19);
 // The game restarts whenever the player wins or loses.
 
 
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// When the game begins again, the player should see a new random number. 
+//Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
 
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
 
-// Each crystal should have a random hidden value between 1 - 12.
